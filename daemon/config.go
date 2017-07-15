4561ceac63c677b0e87bdfb8870154695fee36e9
Peter Borodatyy <peterborodatyy@gmail.com>
2017-07-15 11:52:40 +0300

    Added struct for all config file.

--- daemon/config.go
+++ daemon/config.go
@@ -7,10 +7,48 @@ import (
 )
 
 type Config struct {
-	System struct {
+	Client struct {
 		Host string `yaml:"listen_host"`
 		Port string `yaml:"listen_port"`
+	} `yaml:"client"`
+	Cluster struct {
+		Host string `yaml:"listen_host"`
+		Port string `yaml:"listen_port"`
+		AcceptHosts []string `yaml:"accept_hosts"`
+	} `yaml:"cluster"`
+	System struct {
+		ConfigsPath string `yaml:"configs_path"`
 	} `yaml:"system"`
+	Storage struct {
+		Driver string `yaml:"driver"`
+		Credentials struct {
+			Host string `yaml:"host"`
+			Port string `yaml:"port"`
+			User string `yaml:"user"`
+			Password string `yaml:"password"`
+		} `yaml:"credentials"`
+	} `yaml:"storage"`
+	DockerSecrets struct {
+		Path string `yaml:"path"`
+	}
+	Metrics struct {
+		Driver string `yaml:"driver"`
+		Credentials struct {
+			Host string `yaml:"host"`
+			Port string `yaml:"port"`
+			User string `yaml:"user"`
+			Password string `yaml:"password"`
+		} `yaml:"credentials"`
+	} `yaml:"metrics"`
+	Logs struct {
+		Driver string `yaml:"driver"`
+		Credentials struct {
+			Host string `yaml:"host"`
+			Port string `yaml:"port"`
+			User string `yaml:"user"`
+			Password string `yaml:"password"`
+		} `yaml:"credentials"`
+	} `yaml:"logs"`
 }
 
 const config_path  = `config/config.yaml`
